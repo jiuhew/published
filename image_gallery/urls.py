7db316d323e0b3762c8258792cdeff34db776f3b
Jiuhe Wang <wjh96369@outlook.com>
2025-04-11 23:21:46 +0800

    modified:   image_gallery/urls.py

--- image_gallery/urls.py
+++ image_gallery/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     3. lalala
     4. dev branch
+    5. dev2 branch
 """
 from django.contrib import admin
 from django.urls import path, include
