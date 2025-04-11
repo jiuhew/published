b4507352423686d1b9704c5219b4a1de8a6ef854
Jiuhe Wang <wjh96369@outlook.com>
2025-04-11 17:59:33 +0800

    new file:   gallery/static/gallery/css/styles.css 	modified:   gallery/templates/gallery/base.html 	modified:   gallery/templates/gallery/home.html 	modified:   gallery/templates/gallery/slideshow.html 	modified:   image_gallery/settings.py

--- image_gallery/settings.py
+++ image_gallery/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # 媒体文件配置
 MEDIA_URL = '/media/'
